bc906393ade6bac2e4e54b386975c51f1ae909e3
Nathan Connor <nconnor@leapfrog.com>
2015-10-16 11:10:36 -0700

    Was using wrong url for api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.api_url = "http://pubs-portal.herokuapp.com"
+  config.api_url = "http://pubs-portal-api.herokuapp.com"
 
   config.force_ssl = true
 end
