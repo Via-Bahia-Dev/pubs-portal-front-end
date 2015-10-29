4788bc7537f3184361725a21547b4f75e4f9c165
David Wosk <dwosk@asperasoft.com>
2015-10-29 14:05:06 -0700

    remove users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-	describe "POST #sign_in" do
-  	it "should display all users" do 
-  		get :index
-  		assert_template :index
-  		assert_template layout: "layouts/application"
-		end
-	end
+	# describe "POST #sign_in" do
+ #  	it "should display all users" do 
+ #  		get :index
+ #  		assert_template :index
+ #  		assert_template layout: "layouts/application"
+	# 	end
+	# end
 
-  describe "GET #index" do
-  	it "should display all users" do 
-  		get :index
-  		assert_template :index
-  		assert_template layout: "layouts/application"
-		end
-	end
+ #  describe "GET #index" do
+ #  	it "should display all users" do 
+ #  		get :index
+ #  		assert_template :index
+ #  		assert_template layout: "layouts/application"
+	# 	end
+	# end
 
 end
