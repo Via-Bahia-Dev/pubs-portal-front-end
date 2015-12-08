b4526b6ed903ea41559becfd387eebc4479adf89
Nathan Connor <nconnor@leapfrog.com>
2015-12-07 17:05:17 -0800

    Fixed looping redirect and trying to get templates when not signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,14 @@
 class WelcomeController < ApplicationController
 
 	def index
-		res = self.class.get("/templates.json", :headers => auth_headers)
-		if res.success?
-			@templates = res.parsed_response["data"]
-		else
-				flash[:error] = res.parsed_response["errors"]
-				redirect_to root_path
+		if signed_in?
+			res = self.class.get("/templates.json", :headers => auth_headers)
+			if res.success?
+				@templates = res.parsed_response["data"]
+			else
+					flash[:error] = res.parsed_response["errors"]
+					# redirect_to root_path
+			end
 		end
 	end
 end
