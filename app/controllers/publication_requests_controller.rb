f873c52e5f15a811d5f9db87f725e6a7f78955f6
Nathan Connor <nconnor@leapfrog.com>
2015-12-15 12:47:31 -0800

    Can now create publication request

--- app/controllers/publication_requests_controller.rb
+++ app/controllers/publication_requests_controller.rb
@@ -12,14 +12,72 @@ class PublicationRequestsController < ApplicationController
 		else
 			@template = nil
 		end
+
+		admin_res = self.class.get("/users/admins", :headers => auth_headers)
+		if admin_res.success?
+			admins = admin_res.parsed_response["data"]
+			@admins_options = admins.map { |user| [ "#{user['first_name']} #{user['last_name']}", user['id']] }
+		else
+			@admins_options = [[]]
+		end
+
+		designer_res = self.class.get("/users/designers", :headers => auth_headers)
+		if designer_res.success?
+			designers = designer_res.parsed_response["data"]
+			@designers_options = designers.map { |user| [ "#{user['first_name']} #{user['last_name']}", user['id']] }
+		else
+			@designers_options = [[]]
+		end
+
+		reviewer_res = self.class.get("/users/reviewers", :headers => auth_headers)
+		if reviewer_res.success?
+			reviewers = reviewer_res.parsed_response["data"]
+			@reviewers_options = reviewers.map { |user| [ "#{user['first_name']} #{user['last_name']}", user['id']] }
+		else
+			@reviewers_options = [[]]
+		end
 	end
 
 	def create
 		params[:publication_request][:user_id] = current_user.id
 		@publication_request = PublicationRequest.new(publication_request_params)
-		# Need to define @template as well
 
-		res = self.class.post("/publication_requests.json", :query => { :publication_request => publication_request_params },
+		# get the template again
+		template_res = self.class.get("/templates/#{params[:publication_request][:template_id]}", :headers => auth_headers)
+		if template_res.success?
+			@template = template_res.parsed_response["data"]
+		else
+			@template = nil
+		end
+
+		admin_res = self.class.get("/users/admins", :headers => auth_headers)
+		if admin_res.success?
+			admins = admin_res.parsed_response["data"]
+			@admins_options = admins.map { |user| [ "#{user['first_name']} #{user['last_name']}", user['id']] }
+		else
+			@admins_options = [[]]
+		end
+
+		designer_res = self.class.get("/users/designers", :headers => auth_headers)
+		if designer_res.success?
+			designers = designer_res.parsed_response["data"]
+			@designers_options = designers.map { |user| [ "#{user['first_name']} #{user['last_name']}", user['id']] }
+		else
+			@designers_options = [[]]
+		end
+
+		reviewer_res = self.class.get("/users/reviewers", :headers => auth_headers)
+		if reviewer_res.success?
+			reviewers = reviewer_res.parsed_response["data"]
+			@reviewers_options = reviewers.map { |user| [ "#{user['first_name']} #{user['last_name']}", user['id']] }
+		else
+			@reviewers_options = [[]]
+		end
+
+		# We need the ActiveRecord of the model in order to add it to the Publication Request association
+		@publication_request.templates << Template.find(params[:publication_request][:template_id])
+		# we merge in a list of the Template model in order to build the association on create
+		res = self.class.post("/publication_requests", :query => { :publication_request => publication_request_params.merge({ templates: [Template.find(params[:publication_request][:template_id])], user_id: current_user.id }) },
 																				 :headers => auth_headers, :detect_mime_type => true)
 		if res.success?
 			flash[:success] = "Request for #{res.parsed_response["data"]["event"]} submitted!"
@@ -34,6 +92,6 @@ class PublicationRequestsController < ApplicationController
 	private
 
 		def publication_request_params
-	    params.require(:publication_request).permit(:event, :description, :dimensions, :rough_date, :due_date, :event_date, :user_id, :admin_id, :reviewer_id, :designer_id, :status)
+	    params.require(:publication_request).permit(:event, :description, :dimensions, :rough_date, :due_date, :event_date, :admin_id, :reviewer_id, :designer_id, :status)
 	  end
 end
