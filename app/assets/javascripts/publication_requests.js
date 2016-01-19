0c83afa77cf7fcc14679a795b11732ade31ba086
Nathan Connor <natyconnor@gmail.com>
2016-01-19 11:56:17 -0800

    Disable/enable comment button if there's text in the form

--- app/assets/javascripts/publication_requests.js
+++ app/assets/javascripts/publication_requests.js
@@ -1,2 +1,14 @@
+$(document).ready(function() {
+	$('#comment').keyup(function() {
+		formatCommentForm();
+	});
+})
 
 
+function formatCommentForm () {
+	if($("#comment").val() == "") {
+		$("#comment-btn").attr('disabled', true);
+	} else {
+		$("#comment-btn").attr('disabled', false);
+	}
+}
