8bbb43f46177c40421659604d387e24ede84f6ae
Nathan Connor <natyconnor@gmail.com>
2016-01-21 13:12:29 -0800

    Delete link now brings up confirmation popup

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,12 +4,26 @@
 $(document).ready(function() {
 	$("#comments").on('click', ".comment-delete", function(e){
 		e.preventDefault();
+	});
+
+	// Popover for comment delete link
+	// using body and selector allows new comments to be selected
+	$("body").popover({
+		selector: '.comment-delete',
+		placement: 'right',
+		html: true,
+		trigger: 'focus',
+		title: "Delete comment?"
+	}).parent().on('click', '#confirm-delete', function() {
 		$.ajax({
-			url: $(this).attr('val'),
+			// the url is stored in the delete link's val
+			// Need to get the parent popover then it's sibling comment-delete
+			url: $(this).parents('div.popover').siblings("a.comment-delete").attr('val'),
 			type: 'DELETE'
 		})
 			.done(function(data) {
 				console.log(data);
+				// find the link with the url and close the parent comment div
 				$("a[val=\""+$(this)[0].url+"\"]").parents('div.comment').slideToggle('slow');
 			})
 			.fail(function(data) {
