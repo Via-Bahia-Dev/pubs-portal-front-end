2da41fc8a4fb07980dce8020a85c40335b8b3b30
Nathan Connor <natyconnor@gmail.com>
2016-02-09 14:39:40 -0800

    Basic styling done for inline editing fields

--- app/assets/javascripts/publication_requests.js
+++ app/assets/javascripts/publication_requests.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
   $.fn.editable.defaults.mode = 'inline';
+  $.fn.editable.defaults.send = 'always';
   $.fn.editable.defaults.ajaxOptions = {type: "PUT"};
 
   $(".editable-field#event-field").editable({
+    display: function(value) {
+      $(this).text(value);
+      $(this).append('<span class="glyphicon glyphicon-pencil overlay-icon"></span>')
+    },
     success: function(response, newValue) {
         $(".page-header h1").html(newValue);
     }
@@ -10,6 +15,10 @@ $(document).ready(function() {
   });
 
   $(".editable-field#dimen-field").editable({
+    display: function(value) {
+      $(this).text(value);
+      $(this).append('<span class="glyphicon glyphicon-pencil overlay-icon"></span>')
+    },
     success: function(response, newValue) {
 
     }
