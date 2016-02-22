af5c850d49ecbe2fe1a986f7feeb5959b1d0c351
Nathan Connor <natyconnor@gmail.com>
2016-02-22 14:22:55 -0800

    Now show the file type for non-image files. Also give an error message if invalid file type is uploaded

--- app/controllers/request_attachments_controller.rb
+++ app/controllers/request_attachments_controller.rb
@@ -4,6 +4,10 @@ class RequestAttachmentsController < ApplicationController
     res = post("/publication_requests/#{params[:publication_request_id]}/request_attachments", {:request_attachment => request_attachment_params })
     if res["errors"].nil?
       render partial: 'show', locals: { attachment: res }
+    else
+      byebug
+      render partial: 'invalid_file', locals: { errors: res["errors"] }
+      # render json: { errors: res["errors"] }, status: :unprocessable_entity
     end
   end
 
@@ -11,7 +15,7 @@ class RequestAttachmentsController < ApplicationController
     res = delete("/request_attachments/#{params[:id]}")
 
     if res['errors'].nil?
-      head :no_content 
+      head :no_content
     else
       flash[:errors] = res['errors']
       render 'show'
