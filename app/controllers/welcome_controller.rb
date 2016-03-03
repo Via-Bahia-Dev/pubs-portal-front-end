3262ae8c3a3022afbf9703c02e4299359be4a498
Nathan Connor <natyconnor@gmail.com>
2016-03-02 18:34:46 -0800

    Template filtering by tags now works

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,9 @@ class WelcomeController < ApplicationController
 	def index
 		if signed_in?
 			@templates = get("/templates")
-			@requests = get("/publication_requests")
-			@statuses = get("/statuses")
+			@requests  = get("/publication_requests")
+			@statuses  = get("/statuses")
+			get_tag_select_options
 		end
 	end
 end
