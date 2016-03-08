e057ea343d29671cae2db3c3b4828b1622e9bdc0
Nathan Connor <natyconnor@gmail.com>
2016-03-07 21:22:40 -0800

    Made x editable textareas 100% width with complex selector

--- app/assets/javascripts/publication_requests.js
+++ app/assets/javascripts/publication_requests.js
@@ -35,7 +35,8 @@ $(document).ready(function() {
   $(".editable-field#descr-field").editable({
     display: function(value) {
       $(this).html(value);
-    }
+    },
+    showbuttons: 'bottom'
   });
 
   $(".editable-field#designer-field").editable({
