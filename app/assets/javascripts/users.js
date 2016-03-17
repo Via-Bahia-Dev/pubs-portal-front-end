4266eaadb9cd891be136ef2e5f367640e3aa15a7
Nathan Connor <natyconnor@gmail.com>
2016-03-17 12:21:45 -0700

    Add editable field validation to user fields

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
   $.fn.editable.defaults.send = 'always';
   $.fn.editable.defaults.ajaxOptions = {type: "PUT"};
 
-  function textFieldDisplay(element, value) {
-    $(element).text(value);
-    $(element).append('<span class="glyphicon glyphicon-pencil overlay-icon"></span>');
-  }
-
   $(".editable-field").editable({
     display: function(value) {
       textFieldDisplay(this, value);
+    },
+    validate: function(value) {
+      return validateRequiredField(value);
     }
   });
 
